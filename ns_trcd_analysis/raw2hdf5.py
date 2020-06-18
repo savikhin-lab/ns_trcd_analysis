d3d5b40a64e29fd533bf48d7510fd490cceceb9e
Zach Mitchell <zmitchell@fastmail.com>
2020-06-18 16:14:05 -0400

    feat: Add progress bar for ingestion

--- ns_trcd_analysis/raw2hdf5.py
+++ ns_trcd_analysis/raw2hdf5.py
@@ -1,3 +1,4 @@
+import click
 import numpy as np
 import h5py
 from itertools import product
@@ -18,17 +19,18 @@ def ingest(input_dir, output_file_path) -> None:
         outfile.create_dataset("data", (20_000, 3, num_shots, len(wls), 1))
         data = outfile["data"]
         outfile.create_dataset("wavelengths", (len(wls),), data=wls)
-        dir_indices = product(range(1, num_shots+1), range(len(wls)))
-        for shot_index, wl_index in dir_indices:
-            print(f"Shot {shot_index}, Wavelength {wl_index}")
-            datadir = input_dir / f"{shot_index}" / f"{wls[wl_index]}"
-            # np.s_[...] generates the indices that you would normally get by slicing a NumPy array
-            par = np.load(datadir / "par.npy")
-            data.write_direct(par, np.s_[:], np.s_[:, 0, shot_index - 1, wl_index, 0])
-            perp = np.load(datadir / "perp.npy")
-            data.write_direct(perp, np.s_[:], np.s_[:, 1, shot_index - 1, wl_index, 0])
-            ref = np.load(datadir / "ref.npy")
-            data.write_direct(ref, np.s_[:], np.s_[:, 2, shot_index - 1, wl_index, 0])
+        dir_indices = [x for x in product(range(1, num_shots+1), range(len(wls)))]
+        with click.progressbar(dir_indices, label="Reading data") as indices:
+            for shot_index, wl_index in indices:
+                datadir = input_dir / f"{shot_index}" / f"{wls[wl_index]}"
+                # np.s_[...] generates the indices that you would normally get by slicing a NumPy array
+                par = np.load(datadir / "par.npy")
+                data.write_direct(par, np.s_[:], np.s_[:, 0, shot_index - 1, wl_index, 0])
+                perp = np.load(datadir / "perp.npy")
+                data.write_direct(perp, np.s_[:], np.s_[:, 1, shot_index - 1, wl_index, 0])
+                ref = np.load(datadir / "ref.npy")
+                data.write_direct(ref, np.s_[:], np.s_[:, 2, shot_index - 1, wl_index, 0])
+        
 
 
 def store_shot(arr, path, shot_idx, wl_idx) -> None:
